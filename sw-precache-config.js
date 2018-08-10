3c311d3f85b43d9437c59be20056c12206bda6f9
rl-king <hello@rubenlieking.com>
2018-08-11 01:12:04 +0200

    Update sw stuff

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,26 +1,27 @@
-const endpoint = 'https://hnpwa.com/api/v0';
+const endpoint = "https://api.hnpwa.com/v0";
+
 
 module.exports = {
-    cacheId: 'elmhnpwa',
-    filename: 'service-worker.js',
+    cacheId: "elmhnpwa",
+    filename: "service-worker.js",
     minify: true,
-    navigateFallback: '/index.html',
+    navigateFallback: "/index.html",
     staticFileGlobsIgnorePatterns: [/manifest\.json$/],
     runtimeCaching: [
         {
             urlPattern: new RegExp(`${endpoint}/(news|newest|ask|show|jobs).json`),
-            handler: 'networkFirst',
-            options: {cache: {maxEntries: 30, name: 'feed-cache'}}
+            handler: "networkFirst",
+            options: {cache: {maxEntries: 30, name: "feed-cache"}}
         },
         {
             urlPattern: new RegExp(`${endpoint}/item/`),
-            handler: 'networkFirst',
-            options: {cache: {maxEntries: 30, name: 'item-cache'}}
+            handler: "networkFirst",
+            options: {cache: {maxEntries: 30, name: "item-cache"}}
         },
         {
             urlPattern: new RegExp(`${endpoint}/user/`),
-            handler: 'networkFirst',
-            options: {cache: {maxEntries: 30, name: 'user-cache'}}
+            handler: "networkFirst",
+            options: {cache: {maxEntries: 30, name: "user-cache"}}
         }
     ]
 };
